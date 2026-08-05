31089fe5d67a89e80784456faa2c89fc78145aec
servicepeachtree-hash <service.peachtree@gmail.com>
2026-08-05 13:26:22 -0800

    Delete peach-tree-next directory

--- peach-tree-next/app/(app)/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-import { supabaseServer } from '@/lib/supabase/server';
-
-export async function POST() {
-  const supabase = supabaseServer();
-  await supabase.auth.signOut();
-  redirect('/login');
-}
